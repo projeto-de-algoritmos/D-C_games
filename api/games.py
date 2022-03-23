a66c90acab20362e00eb7a39971033aa4f7dff0e
Gabriel Carvalho <gabriel315df@gmail.com>
2022-03-22 21:10:50 -0300

    Add initial games

---
+++ api/games.py
@@ -0,0 +1,32 @@
+games = [
+  {
+    'name': 'The Witcher 3: Wild Hunt',
+    'img': 'https://static.wikia.nocookie.net/witcher/images/c/c8/TheWitcher3BoxArt.png/revision/latest?cb=20150512225156',
+    'desc': 'The Witcher 3: Wild Hunt é um jogo eletrônico de RPG de ação em mundo aberto desenvolvido pela CD Projekt RED e lançado no dia 19 de maio de 2015 para as plataformas Microsoft Windows, PlayStation 4, Xbox One e em outubro de 2019 para o Nintendo Switch, sendo o terceiro título da série de jogos The Witcher.',
+    'characteristics': {1:3, 2:5, 3:1, 4:2, 5:4, 6:6},
+  },
+  {
+    'name': 'The Legend of Zelda: Breath of the Wild',
+    'img': 'https://carrefourbr.vtexassets.com/arquivos/ids/7001411-800-auto?v=637347955454600000&width=800&height=auto&aspect=true',
+    'desc': 'The Legend of Zelda: Breath of the Wild é um jogo eletrônico de ação-aventura desenvolvido pela Nintendo Entertainment Planning & Development e publicado pela Nintendo. É o décimo nono título da série The Legend of Zelda e foi lançado mundialmente para Wii U e Nintendo Switch em 3 de março de 2017.',
+    'characteristics': {1:3, 2:2, 3:1, 4:5, 5:4, 6:6},
+  },
+  {
+    'name': 'The Last of Us Part II',
+    'img': 'https://upload.wikimedia.org/wikipedia/pt/9/96/The_Last_of_Us_2_capa.png',
+    'desc': 'The Last of Us Part II é um jogo eletrônico de ação-aventura desenvolvido pela Naughty Dog e publicado pela Sony Interactive Entertainment. É o segundo jogo da franquia e foi lançado em 19 de junho de 2020 exclusivamente para PlayStation 4.',
+    'characteristics': {1:3, 2:1, 3:2, 4:5, 5:6, 6:4},
+  },
+  {
+    'name': 'Resident Evil Village',
+    'img': 'https://image.api.playstation.com/vulcan/ap/rnd/202101/0812/FkzwjnJknkrFlozkTdeQBMub.png',
+    'desc': 'Resident Evil: Village, conhecido no Japão como Biohazard: Village é um jogo eletrônico de survival horror desenvolvido e publicado pela Capcom. É a sequência de Resident Evil 7: Biohazard, de 2017, e foi anunciado pela primeira vez no evento de revelação do PlayStation 5 em 11 de junho de 2020.',
+    'characteristics': {1:1, 2:3, 3:2, 4:5, 5:4, 6:6},
+  },
+  {
+    'name': 'Red Dead Redemption 2',
+    'img': 'https://image.api.playstation.com/cdn/UP1004/CUSA03041_00/Hpl5MtwQgOVF9vJqlfui6SDB5Jl4oBSq.png',
+    'desc': 'Red Dead Redemption 2 é um jogo eletrônico de ação-aventura desenvolvido e publicado pela Rockstar Games. É o terceiro título da série Red Dead e uma prequela de Red Dead Redemption, tendo sido lançado em outubro de 2018 para PlayStation 4 e Xbox One e em novembro de 2019 para Microsoft Windows e Google Stadia.',
+    'characteristics': {1:3, 2:1, 3:6, 4:2, 5:5, 6:4},
+  },
+]
--- api/server.py
+++ api/server.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request
+from games import games
 import sys
-
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "hello"
+    return {'games': games}
 
 
 @app.route('/api', methods=['POST'])
 def addrecord(name):
     return {}
+
+if __name__ == "__main__":
+    app.run()
